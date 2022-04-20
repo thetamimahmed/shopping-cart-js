ca7e564c1a14952f99ac270a3f617a556a5af724
thetamimahmed <wwwtamim000@gmail.com>
2022-04-21 00:03:04 +0600

    price calculate with count

--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
-function countQuantity(countId, isAdd){
+function countQuantity(countId, isAdd, priceId, price){
     const countFieldElement = document.getElementById(countId);
-    const countFieldValue = parseInt(countFieldElement.value);
+    let countFieldValue = parseInt(countFieldElement.value);
     if(isAdd == true){
-        const countNumber = countFieldValue + 1;
-        countFieldElement.value = countNumber;
+        countFieldValue  = countFieldValue + 1;
+        countFieldElement.value = countFieldValue;
     }else{
-        const countNumber = countFieldValue - 1;
-        countFieldElement.value = countNumber;
+        countFieldValue = countFieldValue - 1;
+        countFieldElement.value = countFieldValue;
     }
+    countFieldValue = countFieldElement.value;
+    //price calculate
+    const productPriceElement = document.getElementById(priceId);
+    const productPrice = countFieldValue * price;
+    productPriceElement.innerText  = productPrice;
 }
 
 //samsung s22 handle event listener
 document.getElementById('plus-s22').addEventListener('click', function(){
-    countQuantity('count-s22', true);
+    countQuantity('count-s22', true, 's22-price', 1132);
 });
 
 document.getElementById('minus-s22').addEventListener('click', function(){
-    countQuantity('count-s22', false);
+    countQuantity('count-s22', false, 's22-price', 1132);
 });
 
 //watch  handle event listener
 document.getElementById('plus-watch').addEventListener('click', function(){
-    countQuantity('count-watch', true);
+    countQuantity('count-watch', true, 'watch-price', 732);
 });
 document.getElementById('minus-watch').addEventListener('click', function(){
-    countQuantity('count-watch', false);
+    countQuantity('count-watch', false, 'watch-price', 732);
 });
 
 
 //motoralaG5 handle event listener
 document.getElementById('plus-g5').addEventListener('click', function(){
-    countQuantity('count-g5', true);
+    countQuantity('count-g5', true, 'g5-price', 562);
 });
 document.getElementById('minus-g5').addEventListener('click', function(){
-    countQuantity('count-g5', false);
+    countQuantity('count-g5', false, 'g5-price', 562);
 });
